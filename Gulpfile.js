203f1578da8f9a5b936e266aada9ea8c6da56f08
Chris Platts <chris@virelai.co.uk>
2018-06-17 22:51:53 +0100

    More stuff

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,98 +11,109 @@ var autoprefixer = require('gulp-autoprefixer');
 var sassdoc = require('sassdoc');
 var browserSync = require('browser-sync').create();
 var nunjucksRender = require('gulp-nunjucks-render');
-var concat      = require('gulp-concat');
+var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
-var siteOutput = './dist';
+
+// Testing Webpack
+var vinylPaths = require('vinyl-paths');
+var gutil = require('gulp-util');
+var gprint = require('gulp-print');
+var del = require('del');
+var webpackStream = require('webpack-stream');
+var webpackConfig = require('./webpack.config.js');
 
 
 // -----------------------------------------------------------------------------
 // Configuration
 // -----------------------------------------------------------------------------
 
-var input = './scss/*.scss';
-var inputMain = './scss/main.scss';
-var output = siteOutput + '/css';
-var inputTemplates = './pages/*.html';
+var input = './';
+var inputTemplates = input + '/pages/**/*.njk';
+var inputPages = input + '/pages/*.njk';
+var inputStyles = input + '/styles/**/*.scss';
+var inputStylesMain = input + '/styles/main.scss';
+var inputImages = input + '/images/**/*';
+var inputFonts = input + '/fonts/**/*';
+var inputScripts = [
+      'scripts/main.js'
+    ];
+
+var output = './dist';
+var outputStyles = output + '/styles';
+var outputImages = output + '/images';
+var outputFonts = output + '/fonts';
+var outputScripts = output + '/scripts';
+
 var sassOptions = { outputStyle: 'expanded' };
 var autoprefixerOptions = { browsers: ['last 2 versions', '> 5%', 'Firefox ESR'] };
-var sassdocOptions = { dest: siteOutput + '/sassdoc' };
+var sassdocOptions = { dest: output + '/sassdoc' };
+
+// -----------------------------------------------------------------------------
+// Scripts (using Webpack)
+// -----------------------------------------------------------------------------
+gulp.task('clean_scripts', () => {
+    return gulp.src(`${outputScripts}*`)
+      .pipe(vinylPaths(del));
+});
+
+gulp.task('scripts', ['clean_scripts'] ,() => {
+    return webpackStream(webpackConfig)
+      .pipe(gulp.dest(`${outputScripts}`));
+      // .pipe(browserSync.stream());
+});
 
 
 // -----------------------------------------------------------------------------
 // Sass compilation
 // -----------------------------------------------------------------------------
-
 gulp.task('sass', function() {
   return gulp
-    .src(inputMain)
+    .src(inputStylesMain)
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest(output))
+    .pipe(gulp.dest(outputStyles))
     .pipe(browserSync.stream());
-});
-
-
-// -----------------------------------------------------------------------------
-// Javascript
-// -----------------------------------------------------------------------------
-
-gulp.task('scripts', function() {
-  return gulp.src([
-	  	'./bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
-	  	'js/main.js'
-  	])
-    .pipe(concat({ path: 'main.js'}))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest(siteOutput + '/js'));
 });
 
 
 // -----------------------------------------------------------------------------
 // Templating
 // -----------------------------------------------------------------------------
-
 gulp.task('nunjucks', function() {
   nunjucksRender.nunjucks.configure(['./templates/']);
-  // Gets .html and .nunjucks files in pages
-  return gulp.src(inputTemplates)
-  // Renders template with nunjucks
+  return gulp.src(inputPages)
   .pipe(nunjucksRender())
-  // output files in dist folder
-  .pipe(gulp.dest(siteOutput))
+  .pipe(gulp.dest(output))
 });
 
 
 // -----------------------------------------------------------------------------
 // Imagemin
 // -----------------------------------------------------------------------------
-
 gulp.task('img', function() {
-  return gulp.src('./img/**/*')
+  return gulp.src(inputImages)
     .pipe(imagemin({
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
       use: [pngquant()]
     }))
-    .pipe(gulp.dest(siteOutput + '/img'));
+    .pipe(gulp.dest(outputImages));
 });
 
 
 // -----------------------------------------------------------------------------
 // Fonts
 // -----------------------------------------------------------------------------
-
-// gulp.task('fonts', function() {
-//   return gulp.src(['./fonts/*'])
-//   .pipe(gulp.dest(siteOutput + '/fonts/'));
-// });
+gulp.task('fonts', function() {
+  return gulp.src([inputFonts])
+  .pipe(gulp.dest(outputFonts));
+});
 
 
 // -----------------------------------------------------------------------------
 // Sass documentation generation
 // -----------------------------------------------------------------------------
-
 gulp.task('sassdoc', function() {
   return gulp
     .src(input)
@@ -110,19 +121,26 @@ gulp.task('sassdoc', function() {
     .resume();
 });
 
+// -----------------------------------------------------------------------------
+// Clean distribution directory
+// -----------------------------------------------------------------------------
+gulp.task('clean', () => {
+    return gulp.src(`${output}*`)
+      .pipe(vinylPaths(del));
+});
+
 
 // -----------------------------------------------------------------------------
 // Watchers
 // -----------------------------------------------------------------------------
-
 gulp.task('watch', function() {
     // Watch the sass input folder for change,
     // and run `sass` task when something happens
-    gulp.watch(input, ['sass']).on('change', function(event) {
+    gulp.watch(inputStyles, ['sass']).on('change', function(event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 
-    gulp.watch('./js/*', ['scripts']).on('change', browserSync.reload);
+    gulp.watch(inputScripts, ['webpack_scripts']).on('change', browserSync.reload);
 
     // Watch nunjuck templates and reload browser if change
     gulp.watch(inputTemplates, ['nunjucks']).on('change', browserSync.reload);
@@ -137,7 +155,7 @@ gulp.task('watch', function() {
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: siteOutput
+      baseDir: output
     }
   });
 });
@@ -147,4 +165,4 @@ gulp.task('browser-sync', function() {
 // Default task
 // -----------------------------------------------------------------------------
 
-gulp.task('default', ['sass', 'nunjucks', 'img', 'scripts', 'watch', 'browser-sync']);
+gulp.task('default', ['sass', 'nunjucks', 'img', 'scripts', 'fonts', 'watch', 'browser-sync']);
