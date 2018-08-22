52b740044d569f3ee96f827560ef3ef0f4fc7d95
Chris Platts <chris@virelai.co.uk>
2018-08-22 18:30:29 +0100

    Fix error messages, again

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -188,7 +188,7 @@ $(function () {
           // Set state of select element
           var value = $select.find('.dropdown__option.selected').attr('data-value')
           $select.removeClass('error')
-          $select.find('label.error').remove()
+          $select.find('div.error').remove()
           $select.find('option').first().removeAttr('selected')
           $select.find('option[value="' + value + '"]').prop('selected', true)
           $select.find('select').val(value)
